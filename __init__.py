4f142cc387524dac9bc7b145fe6595460ae6f157
ananya <kango@arch.ethz.ch>
2026-02-25 14:27:24 +0100

    bugfix

--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Reciprocal Frame from COMPAS Mesh Dual
+
+A COMPAS-based solver for generating reciprocal frame structures from triangulated meshes.
 """
-from .core_graph import (
-    ReciprocalFrame,
-    reciprocal_from_mesh
-)
+
+
+from .core_graph import ReciprocalFrame, reciprocal_from_mesh
+
+__all__ = ["ReciprocalFrame", "reciprocal_from_mesh"]
